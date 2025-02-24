7cf43cb34bbb0d8fbb01e4e6ab565726ec0c01e0
Saga690 <master.ayush.2003@gmail.com>
2025-02-24 13:14:35 +0530

    fixes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ app = FastAPI(lifespan=lifespan)
 origins = [
     "http://localhost:3000",  
     "http://127.0.0.1:3000",
-    "https://newsify-frontend.vercel.app/",
+    "https://newsify-frontend.vercel.app",
     "https://newsify-frontend-ayushs-projects-d8e7e17a.vercel.app/"
 ]
 
